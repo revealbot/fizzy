27ac5b1fc124f21ea9758f5a9143830266e453a6
Andy Smith <andy@37signals.com>
2025-08-27 13:55:54 -0500

    Menu on notification settings page

--- app/controllers/notifications/settings_controller.rb
+++ app/controllers/notifications/settings_controller.rb
@@ -1,4 +1,8 @@
 class Notifications::SettingsController < ApplicationController
+  include FilterScoped
+
+  enable_collection_filtering only: :show
+
   def show
     @collections = Current.user.collections.alphabetically
   end
